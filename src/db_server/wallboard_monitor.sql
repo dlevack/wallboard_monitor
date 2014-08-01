b720c2fc68766024f2b8799b42f51fc464a347c7
dlevack <dan@levack.net>
2014-08-01 14:47:34 -0700

    Added Status_Table to database along with data.

--- src/db_server/wallboard_monitor.sql
+++ src/db_server/wallboard_monitor.sql
@@ -11,3 +11,14 @@ create table Host_Table (HOST_ID int auto_increment not null,
 			 HOST_IP varchar(15) not null,
 			 HOST_STATUS int not null default 1,
                          Primary Key(HOST_ID));
+
+create table Status_Table (STATUS_ID in auto_increment not null,
+       	     		   STATUS_NAME varchar(10) not null,
+			   STATUS_COLOR varchar(10) not null,
+			   Primary Key(STATUS_ID));
+
+insert into Status_Table values ('','Unknown','clear');
+insert into Status_Table values ('','Disabled','blue');
+insert into Status_Table values ('','OK','green');
+insert into Status_Table values ('','Warning','yellow');
+insert into Status_Table values ('','Critical','red');
