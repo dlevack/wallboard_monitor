c5ac1ec1fa4df816294ce0a414f51167a8c201e0
dlevack <dan@levack.net>
2014-08-11 10:35:32 -0700

    Added host option to allow for installing on remote hosts as well as localhost.

--- src/db_server/install_db.sh
+++ src/db_server/install_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo -n "     Database host: "
+read HOST
 echo -n "Database root user: "
 read USER
 echo -n "          Password: "
@@ -6,19 +8,19 @@ stty -echo
 read PASS
 stty echo
 echo "";
-DB_COUNT=`mysqlshow -u $USER -p$PASS |grep wallboard_db|wc -l`
+DB_COUNT=`mysqlshow -h $HOST -u $USER -p$PASS |grep wallboard_db|wc -l`
 echo "Found $DB_COUNT databases"
 
 if [ $DB_COUNT == 0 ]
 then
-    mysql -u $USER -p$PASS < wallboard_monitor.sql
+    mysql -h $HOST -u $USER -p$PASS < wallboard_monitor.sql
 else
     echo -n "Database exists! Would you like to drop and recreate? [y/n]: "
     read RECREATE
     if [ $RECREATE == 'y' ] 
     then
-	mysql -u $USER -p$PASS < drop_db.sql
-	mysql -u $USER -p$PASS < wallboard_monitor.sql
+	mysql -h $HOST -u $USER -p$PASS < drop_db.sql
+	mysql -h $HOST -u $USER -p$PASS < wallboard_monitor.sql
     else
 	echo "Unable to create database. Please edit SQL file to change database name."
     fi
